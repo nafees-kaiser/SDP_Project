aead60f26f7324a8e521b9d0bb4805c0da42ce71
Nafees Kaiser <oyonafees2001@gmail.com>
2023-09-20 10:12:22 +0600

    Updated sign in page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import SignIn from './Sign_in.jsx'
-import Login from './Login.jsx'
 import Registration from './Registration.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Registration/>
+    {/* <Registration/> */}
+    <SignIn/>
   </React.StrictMode>,
 )
