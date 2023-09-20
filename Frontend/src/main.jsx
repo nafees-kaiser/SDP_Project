96774243a16f1abc2afb798853feb098466013b4
Nafees Kaiser <oyonafees2001@gmail.com>
2023-09-20 09:57:48 +0600

    Deleted some files

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,14 +4,11 @@ import App from './App.jsx'
 
 import SignIn from './Sign_in.jsx';
 import LogIn from './Login.jsx';
-import Verification from './verification.jsx';
-import ChangePass from './ChangePass.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <SignIn/> */}
     <LogIn/>
-    {/* <Verification/> */}
-    {/* <ChangePass/> */}
+   
   </React.StrictMode>,
 )
