30483b6cc6fe44b35ff614de2b1a0c88ff9c3f02
Shahabuddin1122 <shavoddin54@gmail.com>
2023-09-29 04:40:11 +0600

    Home_Seller added

--- Frontend/src/Home_seller.jsx
+++ Frontend/src/Home_seller.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React,{ useState } from "react";
+import { Line, Pie } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    LineElement,
+    CategoryScale,
+    PointElement,
+    LinearScale,
+    ArcElement,
+    Tooltip,
+    Legend
+} from 'chart.js'
 import Style from './Home_seller.module.css';
 import Navbar from "./Components/Navbar_seller";
+import Card from "./Components/Card";
 
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    ArcElement,
+    Tooltip,
+    Legend
+)
 const Home_seller = ()=>{
+    const [salesData] = useState([
+        { no: 1, customerName: 'Customer 1', productName: 'Product A', quantity: 5, price: 10 },
+        { no: 2, customerName: 'Customer 2', productName: 'Product B', quantity: 3, price: 15 },
+        { no: 3, customerName: 'Customer 2', productName: 'Product B', quantity: 3, price: 15 },
+        { no: 4, customerName: 'Customer 2', productName: 'Product B', quantity: 3, price: 15 },
+    ]);
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May','June','July','August','September','October','November','December'],
+        datasets: [
+          {
+            data: [12, 19, 3, 5, 2,24,14,34,45,6,26,34],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 4,
+            tension: 0.5
+          }]
+    };
+    const options = {
+        Plugin:{
+            legend: true,
+            position: "bottom"
+        },
+        title: {
+            text: "Comparison by month",
+            display: true,
+            fontSize: 10
+        }
+
+    }
+    const pieChartData = {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [
+            {
+                data: [12, 19, 3, 5, 2],
+                backgroundColor: ['#eb9360', '#d9bb6a', '#b359eb', '#e866c1', '#66e87e'],
+                borderWidth: 1
+            }
+        ]
+    };
+    
+
     return (
         <>
             <Navbar/>
@@ -58,10 +120,102 @@ const Home_seller = ()=>{
                     </div>
                     
                 </div>
-                <div className={Style.right}></div>
+                <div className={Style.right}>
+                    <Line data={data} options={options} className={Style.graph}></Line>
+
+                </div>
             </div>
             <div className={Style.line}></div>
-            <div className={Style.middle}></div>
+            <div className={Style.middle}>
+                <div className={Style.left}>
+                    <p>Top Tranding Product</p>
+                    <div className={Style.inner}>
+                        <Card
+                            image={'./images/262766466_414622610232846_4583703495738761399_n.jpg'}
+                            review={'cdfhfhf'}
+                            productName={'bfhjbjc'}
+                            location={'ffbhffh'}
+                            price={'jhcjbhdcbj'}
+                        />
+                        <Card
+                            image={'./images/262766466_414622610232846_4583703495738761399_n.jpg'}
+                            review={'cdfhfhf'}
+                            productName={'bfhjbjc'}
+                            location={'ffbhffh'}
+                            price={'jhcjbhdcbj'}
+                        />
+                        <Card
+                            image={'./images/262766466_414622610232846_4583703495738761399_n.jpg'}
+                            review={'cdfhfhf'}
+                            productName={'bfhjbjc'}
+                            location={'ffbhffh'}
+                            price={'jhcjbhdcbj'}
+                        />
+                    </div>
+                </div>
+                <div className={Style.right}>
+                    <Pie
+                        data ={pieChartData}
+                        options={options}
+                    >
+                    </Pie>
+                </div>
+            </div>
+            <div className={Style.line}></div>
+            <div className={Style.customer}>
+                <div className={Style.orders}>
+                    <p>latest orders</p>
+                    <div className={Style.table}>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Customer Name</th>
+                                <th>Product Name</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {salesData.map((sale, index) => (
+                            <tr key={index}>
+                                <td>{sale.no}</td>
+                                <td>{sale.customerName}</td>
+                                <td>{sale.productName}</td>
+                                <td>{sale.quantity}</td>
+                                <td>{sale.price}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+
+                    </div>
+                </div>
+                <div className={Style.topcustomer}>
+                    <p>Top Customers</p>
+                    <div className={Style.table}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Customer Name</th>
+                                    <th>No. of Order</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {salesData.map((sale, index) => (
+                                <tr key={index}>
+                                    <td>{sale.no}</td>
+                                    <td>{sale.customerName}</td>
+                                    <td>{sale.quantity}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
