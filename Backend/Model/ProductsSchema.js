c2f4949c6fb68d7e0ab52e82cae4245f1852b5d4
Mayome <tahsina.mayome0703@gmail.com>
2023-11-15 09:37:21 +0600

    Update Add Product

--- Backend/Model/ProductsSchema.js
+++ Backend/Model/ProductsSchema.js
@@ -24,7 +24,34 @@ const productsSchema = new mongoose.Schema({
     description: {
         type: String,
         required: false
-    }
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    size: {
+        dimention: [{
+            length: String,
+            width: String,
+            height: String,
+            diameter: String
+        }],
+        other: {
+            type: [String],
+            default: null,
+            enum: ["XXS", "XS", "S", "M", "L", "XXL", "XL","5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+        }
+    },
+    color: [String],
+    Product_img1: {
+        type: String
+    },
+    Product_img2: {
+        type: String
+    },
+    Product_img3: {
+        type: String
+    },
 });
 
 const Products = mongoose.model("Products", productsSchema); // Corrected model name
