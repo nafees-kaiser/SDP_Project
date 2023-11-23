9dbe3c58de401ab4a3a1b52ec4db8276c08fcb17
Shahabuddin1122 <shavoddin54@gmail.com>
2023-11-23 13:35:34 +0600

    Community Completed

--- Backend/routes/community.js
+++ Backend/routes/community.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const http = require('http')
+const multer = require('multer'); 
+const path = require('path');
 //const {Server} = require('socket.io')
 const cors = require('cors')
+const cloudinary = require('../cloudinary');
 const router = express.Router();
 const app=express()
 app.use(cors())
 const Community = require('../Model/CommunitySchema');
 const Seller = require('../Model/SellerSchema');
 const Buyer = require('../Model/BuyerSchema');
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); 
+    },
+});
+const upload = multer({ storage: storage });
+
 const io= require('socket.io')(8090,{
     cors: {
         origin: 'http://localhost:5173',
     }
 });
 let users = []
-/*
+
 io.on("connection",(socket) =>{
     console.log('User Connected ',socket.id)
     socket.on('addUser',userId =>{
@@ -28,29 +38,16 @@ io.on("connection",(socket) =>{
         console.log(users)
         
     });
-    socket.on('sendMessage',({senderId,receiverId,message,conversationId})=>{
-        
-        const receiver = users.find(user => user.userId === receiverId);
+    socket.on('sendCommunity',({senderId,message,attachment,date})=>{
         const sender = users.find(user => user.userId === senderId);
-        console.log("SENDER",sender);
-        console.log("Receiver",receiver);
-        if (receiver) {
-            io.to(receiver.socketId).to(sender.socketId).emit('getMessage',{
-                senderId,
-                message,
-                conversationId,
-                receiverId
-            });
-        }
-        else if(sender) {
-            io.to(sender.socketId).emit('getMessage',{
-                senderId,
-                message,
-                conversationId,
-                receiverId
-            });
-        }
+        io.emit('communityMessage', { senderId, message, attachment, date });
     });
+
+
+
+
+
+
     socket.on('sendMessage_seller',({senderId,receiverId,message,conversationId})=>{
         
         const receiver = users.find(user => user.userId === receiverId);
@@ -80,16 +77,52 @@ io.on("connection",(socket) =>{
         io.emit('getUsers',users);
     })
 })
-*/
 
-router.post('/',async (req,res)=>{
+
+
+
+router.post('/', upload.single('img'),async (req,res)=>{
     try {
-        //console.log(req.body)
-        const {senderId,message,attachment} = req.body;
-        const date_T = new Date();
-        const newCommunity = new Community({senderId,message,attachment,date:date_T});
-        await newCommunity.save();
-        res.send("Message sent successfully");
+
+        const userData= req.body;
+        async function run(img) {
+            try {
+              const response = await cloudinary.uploader.upload(img);
+              return response.url;
+            } catch (error) {
+              console.error('Error uploading to Cloudinary:', error);
+              return null;
+            }
+        }
+        if(req.file?.path){
+            run(req.file.path)
+            .then( async (imgpath)=>{
+                const date_T = new Date();
+                console.log(imgpath)
+                const newCommunity = new Community({
+                    ...userData,
+                    attachment:imgpath,
+                    date:date_T
+                });
+                const value= await newCommunity.save();
+                console.log(value)
+                res.status(200).json(value);
+            })
+            .catch((err)=>{
+                console.log(err);
+                res.status(500).send(err);
+            })
+        }
+        else {
+            const date_T = new Date();
+            const newCommunity = new Community({
+                ...userData,
+                date:date_T
+            });
+            const value= await newCommunity.save();
+            res.status(200).send(value);
+        }
+        
     } catch (error) {
         console.log(error,"Error")
     }
@@ -117,62 +150,5 @@ router.get('/',async (req,res)=>{
         console.log(error,"Error")
     }
 })
-/*
-router.post('/conversation', async (req, res) => {
-    try {
-        const { senderId, receiverId } = req.body;
-        const check = await Conversation.find({ members: [senderId, receiverId] });
 
-        if (check.length > 0) {
-            res.status(409).send('Conversation already exists');
-        } else {
-            const newConversation = new Conversation({ members: [senderId, receiverId] });
-            await newConversation.save();
-            res.status(201).send('Conversation created successfully');
-        }
-    } catch (error) {
-        console.error(`Error while posting conversation\n ${error}`);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-router.get('/conversation/:id', async (req, res) => {//id holo buyer er
-    try {
-        const userId = req.params.id;
-        
-        const conversation = await Conversation.find({members:{$in: [userId] } });
-        //console.log(conversation)
-        const conversationUserData = Promise.all(conversation.map(async (conversation) => {
-            const receiverId = await conversation.members.find((member) => member !== userId);
-            const user= await Seller.findById(receiverId);
-            return {
-                user:{id:user.id,email:user.email,fullName:user.name, image:user.img},
-                conversationId: conversation._id
-            }
-        }))
-        res.json(await conversationUserData)
-    } catch (error) {
-        console.log(`Error while getting conversation\n ${error}`)
-    }
-});
-router.get('/conversation_seller/:id', async (req, res) => {//id holo Seller er
-    try {
-        const userId = req.params.id;
-        
-        const conversation = await Conversation.find({members:{$in: [userId] } });
-        //console.log(conversation)
-        const conversationUserData = Promise.all(conversation.map(async (conversation) => {
-            const receiverId = await conversation.members.find((member) => member !== userId);
-            const user= await Buyer.findById(receiverId);
-            return {
-                user:{id:user.id,email:user.email,fullName:user.name, image:user.img},
-                conversationId: conversation._id
-            }
-        }))
-        res.json(await conversationUserData)
-    } catch (error) {
-        console.log(`Error while getting conversation\n ${error}`)
-    }
-});
-*/
 module.exports = router;
