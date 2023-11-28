3ff09b662f6316b7785e43df3e02c45d2e07c829
Nafees Kaiser <oyonafees2001@gmail.com>
2023-11-29 04:25:09 +0600

    Hosted in firebase

---
+++ Frontend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3Ns4WJKoNui9hrlEI3KWjbwVW80K4rec",
+  authDomain: "heritage-f8de0.firebaseapp.com",
+  projectId: "heritage-f8de0",
+  storageBucket: "heritage-f8de0.appspot.com",
+  messagingSenderId: "1003006074424",
+  appId: "1:1003006074424:web:58372d2eb3292cc22310cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
